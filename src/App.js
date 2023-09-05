12c1e968471afa28b061af2e15cdae1a43f649ce
1manity <1052116648@qq.com>
2023-09-05 11:55:22 +0800

    material-ui-study

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import TourHome from "../../material-ui-study/src/Tour/TourHome";
+import TourPage from "../../material-ui-study/src/Tour/TourPage";
+import TourBar from "../../material-ui-study/src/Tour/TourBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // <div>
+      //     {/*<ChatRoom></ChatRoom>*/}
+      //     {/*<Contact />*/}
+      //
+      // </div>
+      <BrowserRouter basename={"/"}>
+        <TourBar />
+        <Routes>
+          <Route path={"/"} element={<TourHome />}></Route>
+          <Route path={"/:id"} element={<TourPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+
   );
 }
 
